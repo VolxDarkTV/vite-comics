a7f1532249fdda7942e73f9b6adc4bcb8a2c9321
VolxDarkTV <vinvenzo.foti@outlook.it>
2022-12-12 18:18:42 +0100

    add Footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,10 +1,144 @@
 <script>
-
+    export default{
+        data(){
+            return{
+                active: 0,
+                links: [
+                    {
+                        text: 'CHARACTERS',
+                        url: '#',
+                        current: false,
+                    },
+                    {
+                        text: 'COMICS',
+                        url: '#',
+                        current: false,
+                    },
+                    {
+                        text: 'MOVIES',
+                        url: '#',
+                        current: false,
+                    },
+                    {
+                        text: 'TV',
+                        url: '#',
+                        current: false,
+                    },
+                    {
+                        text: 'GAMES',
+                        url: '#',
+                        current: false,
+                    },
+                    {
+                        text: 'COLLECTIBLES',
+                        url: '#',
+                        current: false,
+                    },
+                    {
+                        text: 'VIDEOS',
+                        url: '#',
+                        current: false,
+                    },
+                    {
+                        text: 'FANS',
+                        url: '#',
+                        current: false,
+                    },
+                    {
+                        text: 'NEWS',
+                        url: '#',
+                        current: false,
+                    },
+                ]
+            }
+        },
+        methods:{
+            linkSelect (index) {    
+                let x = index;
+                this.active = x;
+            },
+        }
+    }
 </script>
 
 <template>
     <section class="background">
         <div class="logo"></div>
+
+        <div class="left_side">
+            <div>
+                <ul>
+                    <h4>DC COMICS</h4>
+                    <li v-for="(link, index) in links" :key="index" @click="linkSelect(index)">
+                        <a :class="active === index ? 'active' : ''" :href="link.url">{{link.text}}</a>
+                        <div class="under_line" :class="active === index ? 'active_under_line' : ''"></div>
+                    </li>
+                </ul>
+                <ul>
+                   <h4>SHOP</h4>
+                   <li>
+                        <a href="">SHOP DC</a>
+                   </li>
+                   <li>
+                        <a href="">SHOP DC COLLECTIBLES</a>
+                   </li>
+                </ul>
+            </div>
+            <ul>
+                <h4>DC</h4>
+                <li>
+                    <a href="">TERMS OF USE</a>
+                </li>
+                <li>
+                    <a href="">PRIVACY POLICY (NEW)</a>
+                </li>
+                <li>
+                    <a href="">AD CHOICES</a>
+                </li>
+                <li>
+                    <a href="">ADVERTISING</a>
+                </li>
+                <li>
+                    <a href="">JOBS</a>
+                </li>
+                <li>
+                    <a href="">SUBSCRIPTIONS</a>
+                </li>
+                <li>
+                    <a href="">TALENT WORKSHOPS</a>
+                </li>
+                <li>
+                    <a href="">CPSC CERTIFICATES</a>
+                </li>
+                <li>
+                    <a href="">RATINGS</a>
+                </li>
+                <li>
+                    <a href="">SHOP HELP</a>
+                </li>
+                <li>
+                    <a href="">CONTACT US</a>
+                </li>
+            </ul>
+            <ul>
+                <h4>SITES</h4>
+                <li>
+                    <a href="">DC</a>
+                </li>
+                <li>
+                    <a href="">MAD MAGAZINE</a>
+                </li>
+                <li>
+                    <a href="">DC KIDS</a>
+                </li>
+                <li>
+                    <a href="">DC UNIVERSE</a>
+                </li>
+                <li>
+                    <a href="">DC POWER VISA</a>
+                </li>
+            </ul>
+        </div>
 
     </section>
 </template>
@@ -27,4 +161,29 @@
         background-image: url(../../public/img/dc-logo-bg.png);
         background-repeat: no-repeat;
     }
+    .left_side{
+        display: flex;
+        justify-content: center;
+        gap: 20px;
+        width: 50%;
+    }
+
+    // Links
+    ul{
+        gap: 30px;
+        list-style: none;
+        // colore testo
+        & h4{
+            margin: 20px 0;
+        }
+        & a{
+            text-decoration: none;
+            color: #5f7080;
+
+            &:hover{
+                text-decoration: underline;
+                color: #7e8b97;
+            }
+        }
+      }
 </style>
