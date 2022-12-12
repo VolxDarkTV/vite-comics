fe04725ddf8d98235294475a23347b8ce39085e0
VolxDarkTV <vinvenzo.foti@outlook.it>
2022-12-12 15:49:57 +0100

    add dynamic PreFooter

--- src/App.vue
+++ src/App.vue
@@ -2,20 +2,23 @@
 <script>
   import Header from './components/Header.vue'
   import MainPage from './components/MainPage.vue'
+  import PreFooter from './components/PreFooter.vue'
 
 
   export default{
     components:{
       Header,
       MainPage,
+      PreFooter,
     }
   }
 </script>
 
 <!-- Diamo una struttura ai components -->
 <template>
- <Header />
- <MainPage />
+ <Header/>
+ <MainPage/>
+ <PreFooter/>
 </template>
 
 <!-- Diamo uno stile ai components -->
---
+++ src/components/PreFooter.vue
@@ -0,0 +1,74 @@
+<script>
+    export default{
+        data(){
+            return{
+                miniPages: [
+                    {
+                        text: 'DIGITAL COMICS',
+                        img: '../../public/img/buy-comics-digital-comics.png'
+                    },
+                    {
+                        text: 'DC MERCHANDISE',
+                        img: '../../public/img/buy-comics-merchandise.png'
+                    },
+                    {
+                        text: 'SUBSCRIPTION',
+                        img: '../../public/img/buy-comics-subscriptions.png'
+                    },
+                    {
+                        text: 'COMIC SHOP LOCATOR',
+                        img: '../../public/img/buy-comics-shop-locator.png'
+                    },
+                    {
+                        text: 'DC POWER VISA',
+                        img: '../../public/img/buy-dc-power-visa.svg'
+                    },
+                ]
+            }
+        }
+    }
+</script>
+
+
+<template>
+    <div class="container">
+
+        <a v-for="(miniPage, index) in miniPages">
+            <div>
+                <img :src="miniPage.img" alt="#">
+            </div>
+
+            <p>{{miniPage.text}}</p>
+        </a>
+
+    </div>
+</template>
+
+
+<style lang="scss" scoped>
+    .container{
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        gap: 50px;
+        height: 200px;
+        background-color: #0282f9;
+
+        & a{
+            width: 200px;
+            display: flex;
+            align-items: center;
+            gap: 10px;
+
+            & div{
+                width: 80px;
+                height: 80px;
+            }
+        }
+    }
+    img{
+        width: 100%;
+        height: 100%;
+        object-fit: contain;
+    }
+</style>
